583dfbf580e2cae717a2348d43bab771b1534f9e
Osvaldo669 <jasonabraxa@icloud.com>
2022-07-06 17:06:59 -0500

    dia 06-July-2022

--- Pruebas.py
+++ Pruebas.py
@@ -17,3 +17,14 @@ print("*************************************************************************
 print(datos.info())
 print("************************************************************************************")
 print(datos.describe())
+
+
+import pandas as pds
+import numpy as np
+import os
+
+datos = pds.read_csv(r"Document\\covid_19_data.csv")
+pais = datos.groupby('Country/Region')[['Confirmed','Deaths']].sum()
+resultado = pais.nlargest(10, 'Confirmed')
+print(resultado)
+resultado.plot(kind='bar')
